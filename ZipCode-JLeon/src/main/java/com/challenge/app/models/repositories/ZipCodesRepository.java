6eee48bb5ccacdf94acc562a43875503bac6658c
JLeon0 <32750445+JLeon0@users.noreply.github.com>
2021-02-18 15:41:15 -0700

    Delete ZipCode-JLeon directory

--- ZipCode-JLeon/src/main/java/com/challenge/app/models/repositories/ZipCodesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.app.models.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.challenge.app.models.Pojo_ZipCode;
-
-public interface ZipCodesRepository extends CrudRepository<Pojo_ZipCode, Integer> {
-
-}
--- ZipCode-JLeon/src/main/java/com/challenge/app/models/service/ZipCodesService.java
+++
@@ -1,7 +0,0 @@
-package com.challenge.app.models.service;
-
-import com.challenge.app.models.Pojo_ZipCode;
-
-public interface ZipCodesService {
-	public Pojo_ZipCode findById(Integer zip_code);
-}
